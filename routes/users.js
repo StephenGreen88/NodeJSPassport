60ef3b5c06524a4eef726a9d6412b534363239bf
Stephen Green <stephengreen1028@gmail.com>
2020-02-07 21:03:39 -0700

    working on validation for the registration form and creating the rendering instructions on register.ejs. I also created a partials folder with a messages.ejs file to render when an error occurs.

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,40 @@ router.get('/login', (req, res) => res.render('login'));
 // Register Page
 router.get('/register', (req, res) => res.render('register'));
 
+// Register Handle
+router.post('/register', (req, res) => {
+    // Deconstructing allows us to pull stuff out from the input on the form
+    const { name, email, password, password2 } = req.body;
+    let errors = []; // <-- Initialize an array, this is part of the validation process
+
+    // Check the required fields
+    if(!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please fill in all the fields '}); // <-- Message returned if the fields aren't filled out
+    }
+
+    // Check to see if passwords match
+    if(password != password2) {
+        errors.push({ msg: 'Passwords do not match '}); // <-- Message returned if the passwords don't match
+    }
+
+    // Check password length
+    if(password.length < 6) {
+        errors.push({ msg: 'Password needs to be 6 or more characters in length'});
+    }
+
+    // If there are errors do the following
+    if(errors.length > 0 ) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    } else {
+        res.send('pass');
+    }
+
+});
+
 module.exports = router;

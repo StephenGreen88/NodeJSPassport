e8efcac06512f2ef34090c84c8a3ad35abd8f5c5
Stephen Green <stephengreen1028@gmail.com>
2020-02-07 22:54:45 -0700

    Finished login page, dashboard/leaderboard, registration, etc.

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,18 @@
 const express = require('express');
 // Use the express route
 const router = express.Router();
+// Bring in the enstureAutheticated
+const {
+    ensureAuthenticated
+} = require('../config/auth');
 
 // Home Page
 router.get('/', (req, res) => res.render('welcome'));
 
 // Dashboard/Leaderboard
-router.get('/dashboard', (req, res) => res.render('dashboard'));
+router.get('/dashboard', ensureAuthenticated, (req, res) => // <-- bringing in ensureAuthenticated 'protects' this route
+    res.render('dashboard', {
+        name: req.user.name
+    }));
 
 module.exports = router;
